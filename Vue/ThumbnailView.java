040986510182d03cff879aefe347dd171fc3230b
Nassim <nassimeu38950@gmail.com>
2025-07-30 18:32:18 -0400

    Création des vues

---
+++ Vue/ThumbnailView.java
@@ -0,0 +1,12 @@
+package Vue;
+import javax.swing.JPanel;
+
+import Model.ImageModel;
+
+public class ThumbnailView extends JPanel{
+    private ImageModel image;
+    
+    public void update(){
+        
+    }
+}
