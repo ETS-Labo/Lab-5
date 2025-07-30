7ce48b44d38e2abf14f304bd6a679914b7fca811
Nassim <nassimeu38950@gmail.com>
2025-07-30 19:07:37 -0400

    Création Singleton command Manager

---
+++ Controller/CommandManager.java
@@ -0,0 +1,20 @@
+package Controller;
+
+public class CommandManager {
+
+    private CommandManager(){
+
+    }
+
+    public void undoLast(){
+
+    }
+
+    public void executeCommand(Command c){
+
+    }
+
+    public CommandManager getInstance(){
+        return new CommandManager();
+    }
+}
