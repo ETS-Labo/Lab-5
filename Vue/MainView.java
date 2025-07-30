42f6311d05cd0e5696fe972302b5d998f5b2c2a9
Nassim <nassimeu38950@gmail.com>
2025-07-30 18:42:33 -0400

    Création MainView

---
+++ Vue/MainView.java
@@ -0,0 +1,13 @@
+package Vue;
+import javax.swing.JFrame;
+
+public class MainView extends JFrame{
+    private PerspectiveView perspectiveView1;
+    private PerspectiveView perspectiveView2;
+    private ThumbnailView thumbnailView; 
+    
+    public void initComponents(){
+        perspectiveView1 = new PerspectiveView();
+
+        }
+}
