9dee79a0ca7ce2b56a6e639cb0b613f67b3f88bf
Nassim <nassimeu38950@gmail.com>
2025-07-30 19:07:57 -0400

    Création command concret

---
+++ Controller/TranslateCommand.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import Model.PerspectiveModel;
+
+public class TranslateCommand implements Command{
+    private PerspectiveModel perspective;
+    private double prevX;
+    private double prevY;
+    private double dx;
+    private double dy;
+
+    public void execute(){
+
+    }
+
+    public void undo(){
+        
+    }
+}
