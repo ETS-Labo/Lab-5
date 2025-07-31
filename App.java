cfb8b62f9afcab4c384fa28e0f4889105efe74e1
Nassim <nassimeu38950@gmail.com>
2025-07-31 11:08:16 -0400

    Ajout de l'app

---
+++ App.java
@@ -0,0 +1,9 @@
+import Vue.MainView;
+
+public class App {
+    public static void main (String[] args){
+        MainView window = new MainView();
+        window.initComponents();
+        window.setVisible(true);
+    }
+}
