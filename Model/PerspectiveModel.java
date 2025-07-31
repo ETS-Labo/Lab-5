8c644cb5cfd4404e8ea67210f6a5a46b47c19627
Nassim <nassimeu38950@gmail.com>
2025-07-31 11:10:31 -0400

    Ajout de méthode pour le modèle

--- Model/PerspectiveModel.java
+++ Model/PerspectiveModel.java
@@ -1,10 +1,37 @@
 package Model;
 
+import java.awt.Point;
 import java.io.Serializable;
 
 import Vue.Observer;
 
-public class PerspectiveModel implements Observable, Serializable{
+public class PerspectiveModel implements Serializable{
+
+    private double scale;
+    private double translateX;
+    private double translateY;
+
+
+    public void zoom(double factor){
+
+    }
+
+    public void translate(double dx, double dy){
+
+    }
+
+    public <PerspectiveData> PerspectiveData getViewData(){
+        return null;
+    }
+
+    public double getScale(){
+        return this.scale;
+    }
+
+    public Point getTranslation(){
+        return null;
+    }
+
 
     public void addObserver(Observer o){
 
@@ -14,7 +41,4 @@ public class PerspectiveModel implements Observable, Serializable{
 
     }
 
-    public void notifyObservers(){
-
-    }
 }   
