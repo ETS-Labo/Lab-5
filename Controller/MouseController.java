167cfdb489a69aaa64cb5e04ef9cf83004edd275
Nassim <nassimeu38950@gmail.com>
2025-07-31 11:08:33 -0400

    Complétion de MouseController.

--- Controller/MouseController.java
+++ Controller/MouseController.java
@@ -1,10 +1,10 @@
 package Controller;
 
-import org.w3c.dom.events.MouseEvent;
+import java.awt.event.MouseEvent;
 
 import Model.PerspectiveModel;
 
-public class MouseController {
+public class MouseController{
     private PerspectiveModel perspective;
     private int lastX;
     private int lastY;
@@ -14,7 +14,8 @@ public class MouseController {
     }
 
     public void mousePressed(MouseEvent e){
-
+        lastX = e.getX();
+        lastY = e.getY();
     }
 
     public void mouseDragged(MouseEvent e){
