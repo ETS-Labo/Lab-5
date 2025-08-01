59d1ea64e2faa7230c1015e970a950db19bcda49
Elyesse Kourdourli <ekourdourli@gmail.com>
2025-08-01 09:52:25 -0400

    feat: add PerspectiveMemento to capture PerspectiveModel state

--- Model/PerspectiveMemento.java
+++ Model/PerspectiveMemento.java
@@ -1,16 +1,28 @@
 package Model;
 
+/**
+ * Snapshot immuable de l'état d'une PerspectiveModel (scale + translation).
+ */
 public class PerspectiveMemento {
-    private double scale;
-    private double translateX;
-    private double translateY;
-    private double[] tab
+    private final double scale;
+    private final double translateX;
+    private final double translateY;
 
-
-    public PerspectiveMemento(){
-        
+    public PerspectiveMemento(double scale, double translateX, double translateY) {
+        this.scale = scale;
+        this.translateX = translateX;
+        this.translateY = translateY;
     }
-    public getState(){
-        return
+
+    public double getScale() {
+        return scale;
+    }
+
+    public double getTranslateX() {
+        return translateX;
+    }
+
+    public double getTranslateY() {
+        return translateY;
     }
 }
