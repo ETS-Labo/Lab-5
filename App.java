829b7b7e2db93597352f363b0f06b51b60a05b83
Nassim <nassimeu38950@gmail.com>
2025-08-04 11:37:37 -0400

    Suppression de la gestion de Thread.

--- App.java
+++ App.java
@@ -1,13 +1,11 @@
-import javax.swing.SwingUtilities;
+
 
 import Vue.MainView;
 
 public class App {
     public static void main (String[] args){
-        SwingUtilities.invokeLater(() -> {
             MainView window = new MainView();
             window.initComponents();
             window.setVisible(true);
-        });
     }
 }
