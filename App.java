c73736b3da19b362b85d55d97d91a69199eebf63
Floraavgs <flora.avgs@gmail.com>
2025-08-05 14:20:16 -0400

    commentaires App

--- App.java
+++ App.java
@@ -1,11 +1,37 @@
 
-// commentaire
+/*
+ * -------------------------------------------------------------------------
+ * Auteurs          : Flora Auvergnas, Nassim Bouchemella, Elyesse Kourdourli
+ * Description      : Point d’entrée de l’application. Crée et affiche
+ *                    la fenêtre principale (MainView).
+ * Cours/Groupe     : Laboratoire 5 LOG121 / Groupe 11
+ * Date             : 05-08-2025
+ * Détails          :
+ *   - Instancie MainView.
+ *   - Appelle initComponents() pour configurer l’interface.
+ *   - Rend la fenêtre visible.
+ * -------------------------------------------------------------------------
+ */
+
 import Vue.MainView;
 
+/**
+ * Classe de démarrage de l’application.
+ * Contient la méthode main qui initialise et affiche la vue principale.
+ */
 public class App {
-    public static void main (String[] args){
-            MainView window = new MainView();
-            window.initComponents();
-            window.setVisible(true);
+
+    /**
+     * Point d’entrée de l’application.
+     *
+     * @param args Arguments de la ligne de commande (non utilisés)
+     */
+    public static void main(String[] args) {
+        // Création de la fenêtre principale
+        MainView window = new MainView();
+        // Initialisation des composants graphiques
+        window.initComponents();
+        // Affichage de la fenêtre
+        window.setVisible(true);
     }
 }
